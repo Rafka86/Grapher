f2029c76f43d9befdf0cdeb1d63d5f2b2ebd45b7
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-08-11 20:25:44 +0900

    Make TokenType enum.

---
+++ app/src/main/java/net/firemiller/grapher/core/TokenType.kt
@@ -0,0 +1,13 @@
+package net.firemiller.grapher.core
+
+/**
+* Created by Rafka on 2017/08/11.
+*/
+enum class TokenType {
+  NUMBER,
+  VARIABLE,
+  CONSTANT,
+  FUNCTION,
+  OPERATOR,
+  BRACKET,
+}
